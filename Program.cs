f9367d6df81c8f7f8caf4f76b9ff86f8fc0a6be1
Sam Hobbs <9809223+SimpleSamples@users.noreply.github.com>
2022-05-20 18:49:58 -0700

    Nearly complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text.Json;
+using static System.Environment;
 
 //var page = new RazorPage
 //{
@@ -8,12 +9,45 @@
 //    ProjectMdl = "CListCtrl"
 //};
 
-const string fileName = "AppSettings.json";
+const string AppSettingsFn = "AppSettings.json";
+const string CshtmlFn = "cshtml.txt";
+const string CshtmlCsFn = "cshtmlcs.txt";
 string? jsonString;
 
 //  *  *  *  *  *  *  *  *
 
-jsonString = File.ReadAllText(fileName);
+// Check for the LocalApplicationData folder and the two files in it
+
+string FolderPath = Environment.GetFolderPath(SpecialFolder.LocalApplicationData);
+string appData = Path.Combine(FolderPath, "RazorPagesGen");
+DirectoryInfo DirInfo = new DirectoryInfo(appData);
+if (!DirInfo.Exists)
+{
+    Console.WriteLine($"The directory {appData} does not exist");
+    return;
+}
+string? fn;
+bool filesexist = true;
+fn = appData + '\\' + CshtmlFn;
+FileInfo cshtmlfi = new FileInfo(fn);
+if (!cshtmlfi.Exists)
+{
+    Console.WriteLine($"The file {fn} does not exist");
+    filesexist = false;
+}
+fn = appData + '\\' + CshtmlCsFn;
+FileInfo cshtmlCsfi = new FileInfo(fn);
+if (!cshtmlCsfi.Exists)
+{
+    Console.WriteLine($"The file {fn} does not exist");
+    filesexist = false;
+}
+if (!filesexist)
+    return;
+
+// Read the settings
+
+jsonString = File.ReadAllText(AppSettingsFn);
 if (jsonString == null)
 {
     Console.WriteLine("Read error");
@@ -21,30 +55,40 @@ if (jsonString == null)
 }
 RazorPage? page = JsonSerializer.Deserialize<RazorPage>(jsonString);
 
-Boolean More =true;
+Boolean More = true;
 while (More)
     ShowPage(page);
-
-return;
-
-jsonString = JsonSerializer.Serialize(page);
-if (jsonString == null)
-{
-    Console.WriteLine("Serialization error");
-    return;
-}
-File.WriteAllText(fileName, jsonString);
 
 //  *  *  *  *  *  *  *  *
 
 void ShowPage(RazorPage? page)
 {
+    Console.Clear();
     Console.WriteLine($"Directory [D]: {page.ProjectDir}");
     Console.WriteLine($"Namespace [N]: {page.ProjectNs}");
     Console.WriteLine($"     Area [A]: {page.ProjectArea}");
     Console.WriteLine($"      Mdl [M]: {page.ProjectMdl}");
-    Console.WriteLine(" Generate [G]");
-    Console.WriteLine("Press the Escape (Esc) key to quit: \n");
+    Console.WriteLine("     Save [S]");
+    string PagesDirInfo = page.ProjectDir + "\\Areas\\" + page.ProjectArea + "\\Pages";
+    DirectoryInfo di = new DirectoryInfo(PagesDirInfo);
+    bool CanGenerate = false;
+    if (!di.Exists)
+    {
+        Console.WriteLine(PagesDirInfo);
+        Console.WriteLine("Doesn't exist yet; modify above parameters some more");
+    }
+    else
+    {
+        FileInfo Cshtmlfi = new FileInfo(di.FullName + '\\' + page.ProjectMdl + ".cshtml");
+        FileInfo CshtmlCsfi = new FileInfo(di.FullName + '\\' + page.ProjectMdl + ".cshtml.cs");
+        if (!Cshtmlfi.Exists && !CshtmlCsfi.Exists)
+        {
+            Console.WriteLine(" Generate [G]");
+            CanGenerate = true;
+        }
+    }
+    Console.WriteLine("Press the Escape (Esc) key to quit\n");
+
     ConsoleKeyInfo response = Console.ReadKey();
     switch (response.Key)
     {
@@ -68,8 +112,82 @@ void ShowPage(RazorPage? page)
             page.ProjectMdl = Console.ReadLine();
             break;
         case ConsoleKey.G:
+            Save();
+            if (CanGenerate)
+            {
+                GenerateTo(page);
+            }
+            break;
+        case ConsoleKey.S:
+            Save();
             break;
     }
+}
+
+void GenerateTo(RazorPage page)
+{
+    string text = string.Empty;
+    // Read Cshtml
+    try
+    {
+        fn = appData + '\\' + CshtmlFn;
+        using (StreamReader sr = new StreamReader(fn))
+        {
+            text = sr.ReadToEnd();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception reading {fn}: {ex.Message}");
+    }
+    // Write Cshtml
+    try
+    {
+        fn = page.ProjectDir + "\\Areas\\" + page.ProjectArea + "\\Pages" + '\\' + page.ProjectMdl + ".cshtml";
+        using (StreamWriter sw = new StreamWriter(fn))
+        {
+            sw.Write(text);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception writing {fn}: {ex.Message}");
+    }
+    // Read Cshtml.cs
+    try
+    {
+        fn = appData + '\\' + CshtmlCsFn;
+        using (StreamReader sr = new StreamReader(fn))
+        {
+            text = sr.ReadToEnd();
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception reading {fn}: {ex.Message}");
+    }
+    // Write Cshtml.cs
+    try
+    {
+        fn = page.ProjectDir + "\\Areas\\" + page.ProjectArea + "\\Pages" + '\\' + page.ProjectMdl + ".cshtml.cs";
+        using (StreamWriter sw = new StreamWriter(fn))
+        {
+            sw.Write(text);
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Exception writing {fn}: {ex.Message}");
+    }
+}
+
+void Save()
+{
+    jsonString = JsonSerializer.Serialize(page);
+    if (jsonString == null)
+        Console.WriteLine("Serialization error");
+    else
+        File.WriteAllText(AppSettingsFn, jsonString);
 }
 
 public class RazorPage
